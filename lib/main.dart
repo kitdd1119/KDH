9c208340498c65a9e0293a5f7d9d4fb28923a1e1
kitdd1119 <kitdd1119@gmail.com>
2023-05-29 19:11:19 +0900

    로그인과 회원가입 UI 구현

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,62 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'M P C',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
 
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('M P C'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              '어서오세요 여기는 고기 가격 비교 전문점입니다.', //Meat Price Comparison shop
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              '정직하고 신선한 고기를 즐겨보세요!',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+            child: Text('장보기'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+/*
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,7 +70,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+*/
 class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -50,8 +105,43 @@ class SignupPage extends StatelessWidget {
             ElevatedButton( // RaisedButton 대신 ElevatedButton으로 변경
               onPressed: () {
                 // 회원가입 버튼을 눌렀을 때 실행될 코드
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignupSuccessPage()),
+                ).then((value) {
+                  // 회원가입 성공 후에 기본 창으로 돌아갈 때 실행될 코드
+                  Navigator.pop(context);
+                });
               },
               child: Text('회원가입'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignupSuccessPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('회원가입 완료'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.check_circle_outline,
+              size: 80.0,
+              color: Colors.green,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '회원가입이 완료되었습니다!',
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
             ),
           ],
         ),
